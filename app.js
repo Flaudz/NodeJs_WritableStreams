7f6a9e2f340a1c7895167b5ac082bba782a253d1
Flaudz <nico936d@edu.campusvejle.dk>
2020-03-14 15:14:49 +0100

    Done so its a html file its pipeing icuz<3

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 
 let server = http.createServer((req, res) => {
 	console.log(`Request was me: ${req.url}`);
-	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.writeHead(200, { "Content-Type": "text/html" });
 
-	const myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+	const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
 	myReadStream.pipe(res);
 });
 
